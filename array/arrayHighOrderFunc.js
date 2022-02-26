a15c51934219a32ea160e35719ba145b6ac1e8c8
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-02-26 21:29:42 +0900

    forEach메서드 연습

--- array/arrayHighOrderFunc.js
+++ array/arrayHighOrderFunc.js
@@ -5,6 +5,13 @@ const cars = [
   { brand: 'BMW', model: 'i3', isFast: false, price: 300 },
 ]; // trailing comma: 배열이나 객체의 마지막 요소에도 쉼표를 찍어주는게 추후 코드 수정에 편리함.
 
+// forEach 메서드:
 cars.forEach(function (car) {
   console.log(car.isFast);
 });
+/*
+false
+true
+true
+false
+*/
