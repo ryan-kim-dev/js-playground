089d48d1d1c2d475cca6061fdfa78323ff906227
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-02-26 22:26:21 +0900

    배열 메서드 map, filter 실습

--- array/arrayHighOrderFunc.js
+++ array/arrayHighOrderFunc.js
@@ -5,8 +5,9 @@ const cars = [
   { brand: 'BMW', model: 'i3', isFast: false, price: 300 },
 ]; // trailing comma: 배열이나 객체의 마지막 요소에도 쉼표를 찍어주는게 추후 코드 수정에 편리함.
 
-// forEach 메서드:
+// forEach 메서드: for문을 대체하는 고차함수. 객체의 길이만큼 반복하며 콜백함수에 반복하며 할 행동을 지정해준다.
 cars.forEach(function (car) {
+  // forEach메서드의 인자로 (요소값, 인덱스, this) 세가지를 인자로 줄 수 있다.
   console.log(car.isFast);
 });
 /*
@@ -15,3 +16,16 @@ true
 true
 false
 */
+
+// map 메서드: 기존 배열을 통해 새로운 배열을 만들 때 사용.
+const carPrice = cars.map(function (element) {
+  return element.price;
+});
+console.log(carPrice); // [ 100, 1000, 500, 300 ]
+
+// filter 메서드: 기존 배열에서 특정 조건만 가진 value만 뽑아냄.
+// 데이터를 불러와서 데이터테이블(라이브러리)을 만들 때 사용. ex: 실시간 검색 등
+const slowCars = cars.filter(function (slow) {
+  return slow.isFast === false;
+});
+console.log(slowCars);
