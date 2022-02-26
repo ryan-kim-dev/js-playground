0d9826d2a7119a52b6122d78891f70a155a53c07
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-02-26 21:28:36 +0900

    배열 고차함수 forEach 메서드 연습

---
+++ array/arrayHighOrderFunc.js
@@ -0,0 +1,10 @@
+const cars = [
+  { brand: 'Hyundai', model: 'Sonata', isFast: false, price: 100 },
+  { brand: 'Benz', model: 'AMG', isFast: true, price: 1000 },
+  { brand: 'Kia', model: 'Stinger', isFast: true, price: 500 },
+  { brand: 'BMW', model: 'i3', isFast: false, price: 300 },
+]; // trailing comma: 배열이나 객체의 마지막 요소에도 쉼표를 찍어주는게 추후 코드 수정에 편리함.
+
+cars.forEach(function (car) {
+  console.log(car.isFast);
+});
