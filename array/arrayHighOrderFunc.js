8542a119ddc8e8165d0f931478e81e0f787681a7
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-02-27 15:34:48 +0900

    reduce 메서드 실습 추가

--- array/arrayHighOrderFunc.js
+++ array/arrayHighOrderFunc.js
@@ -1,3 +1,6 @@
+// 배열 고차 함수 forEach, map, filter, reduce 학습
+// 참고 강의: https://www.youtube.com/watch?v=vqdzVZxoRtM&t=19s
+
 const cars = [
   { brand: 'Hyundai', model: 'Sonata', isFast: false, price: 100 },
   { brand: 'Benz', model: 'AMG', isFast: true, price: 1000 },
@@ -6,8 +9,8 @@ const cars = [
 ]; // trailing comma: 배열이나 객체의 마지막 요소에도 쉼표를 찍어주는게 추후 코드 수정에 편리함.
 
 // forEach 메서드: for문을 대체하는 고차함수. 객체의 길이만큼 반복하며 콜백함수에 반복하며 할 행동을 지정해준다.
+// forEach메서드의 매개변수에 요소값, 인덱스, this 세가지를 넣어 인자로 받을 수 있다.
 cars.forEach(function (car) {
-  // forEach메서드의 인자로 (요소값, 인덱스, this) 세가지를 인자로 줄 수 있다.
   console.log(car.isFast);
 });
 /*
@@ -16,10 +19,13 @@ true
 true
 false
 */
+cars.forEach(function (car, index) {
+  console.log('forEach 메서드 ' + car.model, index);
+});
 
-// map 메서드: 기존 배열을 통해 새로운 배열을 만들 때 사용.
+// map 메서드: 기존 배열의 특정 key의 value들로 새로운 배열을 만들 때 사용.
 const carPrice = cars.map(function (element) {
-  return element.price;
+  return `The ${element.model}'s price is ${element.price} and isFast value is ${element.isFast}.`;
 });
 console.log(carPrice); // [ 100, 1000, 500, 300 ]
 
@@ -29,3 +35,10 @@ const slowCars = cars.filter(function (slow) {
   return slow.isFast === false;
 });
 console.log(slowCars);
+
+// reduce 메서드:
+const totalPrice = cars.reduce(function (acc, cur) {
+  // 초기값과 배열의 첫번째 요소값을 콜백함수에 인수로 전달.
+  return acc + cur.price; // obj + obj.value;
+}, 0); // 초기값(acc)를 0으로 반드시 초기화 시켜줘야 정상적으로 출력.
+console.log(totalPrice);
