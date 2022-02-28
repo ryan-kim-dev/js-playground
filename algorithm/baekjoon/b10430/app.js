be00c12ea30002a2b0c1ec3ac2b2662fccf51c63
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-02-28 16:16:05 +0900

    백준 10430번 문제풀이

---
+++ algorithm/baekjoon/b10430/app.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+input = input[0];
+input = input.split(' ').map((item) => +item);
+
+let A = input[0],
+  B = input[1],
+  C = input[2];
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
