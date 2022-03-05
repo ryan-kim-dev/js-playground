946c7d0849bc11d5a0dab36ad3f6386a3213f430
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-03-05 10:54:14 +0900

    input값 정제하는 좋은 방식 주석에 첨부

--- algorithm/baekjoon/b10950/app.js
+++ algorithm/baekjoon/b10950/app.js
@@ -16,3 +16,14 @@ function solution(sum) {
   }
 }
 solution(input);
+// const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+// let input = fs.readFileSync(filePath).toString().split('\n');
+// const testCaseArray = [];
+// for (let i = 1; i <= +input[0]; ++i) {
+//   let tempValue = input[i].split(' ').map((item) => +item);
+//   testCaseArray.push({ A: tempValue[0], B: tempValue[1] });
+// }
+// console.log(testCaseArray);
+// function solution(T, testCaseArray) {}
+// // solution(+input[0], testCaseArray);
